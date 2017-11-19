828f09890b70b2e524701f9ce5084dfbf07932e0
IrishCyborg <finn.headen@gmail.com>
2017-11-19 14:08:20 +1100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cloud Demo' });
+});
+router.get('/demo', function(req, res, next) {
   res.render('demo', { title: 'Cloud Demo' });
 });
 
